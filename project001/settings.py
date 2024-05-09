00e00d70ec2f967775ce3dfaeedda8fbd7d1704e
maidul02 <maidulislammahin02@gmail.com>
2024-05-10 03:12:42 +0600

    Fixed versions for requirements.txt

--- project001/settings.py
+++ project001/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v@@+*q$=(1(1b)wc5lt$^+myt5%+vtngvget489pc#b$w2yr8=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 
 
 # Application definition
